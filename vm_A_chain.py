78bb66baa916f4bb871e5aa40bf05d54480a5706
mayykhan <maykhan@usc.edu>
2025-10-02 05:43:41 +0000

    part 2 done

--- vm_A_chain.py
+++ vm_A_chain.py
@@ -2,15 +2,19 @@ import paho.mqtt.client as mqtt
 import time
 from datetime import datetime
 import socket
-a = 0
+
 def on_message(client, userdata, message):
-    print("Pong Callback - pong: " + int(message.payload.decode()))
+    print("A: Pong Received - pong: " + message.payload.decode())
     a = int(message.payload.decode()) + 1
     client.publish("maykhan/ping", str(a))
+    print("A: Ping Published - ping: {a}")
 
 if __name__ == '__main__':
     client = mqtt.Client()
     client.connect("192.168.xx.xx", port=1883, keepalive=60)
     client.subscribe("maykhan/pong")
+    client.on_message=on_message
     client.publish("maykhan/ping", "67")
+    print("A: Ping Published - ping: 67")
+
     client.loop_forver()
--- vm_B_chain.py
+++ vm_B_chain.py
@@ -3,15 +3,16 @@ import time
 from datetime import datetime
 import socket
 
-a = 0
 
 def on_message(client, userdata, message):
-    print("Ping Callback - ping: " + int(message.payload.decode()))
+    print("B: Ping Received - ping: " + message.payload.decode())
     a = int(message.payload.decode()) + 1
     client.publish("maykhan/pong", str(a))
+    print("B: Pong Published - pong: {a}")
 
 if __name__ == '__main__':
     client = mqtt.Client()
     client.connect("192.168.xx.xx", port=1883, keepalive=60)
     client.subscribe("maykhan/ping")
-    client.loop_forver()
+    client.on_message=on_message
+    client.loop_forever()
