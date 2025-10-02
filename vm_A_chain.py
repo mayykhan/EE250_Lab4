44a0a1bebe7b716c7267f582f6daebcbd3af3fb3
Shreya Nair <shreyana@usc.edu>
2025-10-02 19:28:44 +0000

    debug part 2

--- vm_A_chain.py
+++ vm_A_chain.py
@@ -7,14 +7,14 @@ def on_message_from_ping(client, userdata, message):
     print("A: Pong Received - pong: " + message.payload.decode())
     a = int(message.payload.decode()) + 1
     client.publish("maykhan/ping", str(a))
-    print("A: Ping Published - ping: {a}")
+    print(f"A: Ping Published - ping: {a}")
 
 if __name__ == '__main__':
     client = mqtt.Client()
-    client.connect("192.168.xx.xx", port=1883, keepalive=60)
+    client.connect("192.169.0.141", port=1883, keepalive=60)
     client.subscribe("maykhan/pong")
-    client.message_callback_add("maykhan/ping", on_message_from_ping)
+    client.message_callback_add("maykhan/pong", on_message_from_ping)
     client.publish("maykhan/ping", "67")
     print("A: Ping Published - ping: 67")
 
-    client.loop_forver()
+    client.loop_forever()
--- vm_B_chain.py
+++ vm_B_chain.py
@@ -8,12 +8,12 @@ def on_message_from_pong(client, userdata, message):
     print("B: Ping Received - ping: " + message.payload.decode())
     a = int(message.payload.decode()) + 1
     client.publish("maykhan/pong", str(a))
-    print("B: Pong Published - pong: {a}")
+    print(f"B: Pong Published - pong: {a}")
 
 if __name__ == '__main__':
     client = mqtt.Client()
-    client.connect("192.168.xx.xx", port=1883, keepalive=60)
+    client.connect("192.169.0.141", port=1883, keepalive=60)
     client.subscribe("maykhan/ping")
-    client.on_message=on_message
+    client.on_message=on_message_from_pong
     client.message_callback_add("maykhan/pong", on_message_from_pong)
     client.loop_forever()
